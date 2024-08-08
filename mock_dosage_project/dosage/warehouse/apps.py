a2cade05ed7e3ba47aa736559049d976225eca72
Martus89 <88802838+martus89@users.noreply.github.com>
2024-08-08 11:39:16 +0200

    Added views and templates

--- mock_dosage_project/dosage/warehouse/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WarehouseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'warehouse'
-    verbose_name = "Warehouse details"
--- mock_dosage_project/dosage/warehouse/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import homepage
-
-urlpatterns = [
-    ]
